8f2b2607d0fee9ee310c4ba7900ca86da7a59b74
Your Personal Name <your.personal@example.com>
2025-03-15 23:14:51 +0100

    Fix pygame initialization and improve game loop structure

--- main.py
+++ main.py
@@ -2,19 +2,21 @@ import pygame # type: ignore
 from constants import *
 
 def main():
-    pygame.init
+    pygame.init()
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
-    i = 0
-    while i == 0:
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
